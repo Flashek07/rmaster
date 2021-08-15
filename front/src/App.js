1a1d60d8bfbfd1292c06b2e436a83e351735caf5
BuildTools <unconfigured@null.spigotmc.org>
2021-08-16 00:23:47 +0200

    main

---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+
+const Navbar = () => {
+  return(
+    <div className="navBar">
+      <div className="navButton" >
+      <button className="button1">ToDo!</button>
+      <button className="button2">Done!</button>
+      <button className="button3">Sign In!</button>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default Navbar;
